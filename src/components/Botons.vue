81107c792178c1a5bddfc9e9df46d904b421b637
tonidevelopez <101570134+tonidevelopez@users.noreply.github.com>
2023-01-27 16:35:47 +0100

    Exercici 5

--- src/components/Botons.vue
+++ src/components/Botons.vue
@@ -11,7 +11,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 button {
     width: 50%;
     padding: 1rem;
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
+    <div v-if="!mostrarEscena" class="benvingut">
+        <h2>Benvingut a aquest tutorial, on trobaràs uns petits consells del nostre heroi</h2>
+        <button @click="start">Començar</button>
+    </div>
+    <div v-else>
         <Botons @previousSentence="previousSentence" @nextSentence="nextSentence" />
         <Escena :frases='frases' :currentSentence="currentSentence" />
     </div>
@@ -32,7 +36,8 @@ export default {
                     id: 4
                 },
             ],
-            currentSentence: 1
+            currentSentence: 1,
+            mostrarEscena: false
         }
     },
     methods: {
@@ -45,7 +50,39 @@ export default {
             if (this.currentSentence !== 4) {
                 this.currentSentence++
             }
+        },
+        start() {
+            this.mostrarEscena = !this.mostrarEscena
         }
     }
 }
 </script>
+
+<style scoped>
+.benvingut {
+    min-height: 40vh;
+    background-color: rgb(222, 99, 65);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-radius: 150px;
+    padding: 50px;
+}
+
+.benvingut h2 {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    color: white;
+    font-weight: bold;
+    margin-top: 20px;
+}
+
+.benvingut button {
+    margin-top: 40px;
+    width: 150px;
+    height: 40px;
+    font-size: 1rem;
+    border-radius: 20px;
+}
+</style>
